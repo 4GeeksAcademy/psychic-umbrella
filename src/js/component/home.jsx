7dcfa3713c34de47c3813c260b361b67d4d5b859
carolinaborr <162605159+carolinaborr@users.noreply.github.com>
2024-12-03 21:01:08 +0000

    ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Container">
+			<h1>My Neobrustalistic Todo List</h1>
+			<ul>
+				<li>
+					<input type="text" onChange={(e) => setInputValue(e.target.value)} value={inputValue} onKeyUp={(e) => {if (e.key === "Enter"){ setTodos(todos.concat([inputValue])); setInputValue("")}}} placeholder="What do you want now!?"></input>
+				</li>
+				{todos.map((item, index) => (
+					<li>
+						{item} {" "} <i className="fa-solid fa-trash-can" onClick={() => setTodos(todos.filter((_, currentIndex) => currentIndex !== index))} />
+
+						
+					</li>
+
+				))}
+			</ul>
 		</div>
 	);
 };
